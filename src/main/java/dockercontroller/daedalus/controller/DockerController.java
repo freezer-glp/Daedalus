21b89c5962eb65186c1319442b05cfb187fe7402
Gong Lingpu <409349942@qq.com>
2015-08-02 16:38:18 +0800

    finish the DockerController

--- src/main/java/dockercontroller/daedalus/controller/DockerController.java
+++ src/main/java/dockercontroller/daedalus/controller/DockerController.java
@@ -1,5 +1,7 @@
 package dockercontroller.daedalus.controller;
 
+import java.util.Scanner;
+
 import org.apache.log4j.Logger;
 
 import dockercontroller.daedalus.manager.ContainerManager;
@@ -8,33 +10,104 @@ public class DockerController {
 	private static ContainerManager manager = new ContainerManager();
 	private Logger logger = Logger.getLogger(DockerController.class);
 	private static final String EXPOSE = "8080/tcp";
+	ContainerManager cm = new ContainerManager();
 	
 	private String inspect(String containerID) {
+	    cm.inspect(containerID);
         return containerID;
 	}
 	
-	public String create() {
+	public String create(String appID,String location) {
+        cm.create(appID, location);
+	    return appID;
+	}
+
+	public String stop(String containerID, Integer time) {
+	    cm.stop(containerID, time);
         return null;
 	}
 
-	public String stop() {
-        return null;
-	}
-
-	public String start() {
+	public String start(String containerID) {
+	    cm.start(containerID);
         return null;
 	}
 
 	public String restart(String containerID, int time)  {
+	    cm.restart(containerID, time);
         return containerID;
 	}
 
-	public boolean remove( ) {
+	public boolean remove(String containerID) {
+	    cm.remove(containerID);
         return false;
 	}
 
-	public boolean forceRemove() {
+	public boolean forceRemove(String containerID) {
+	    cm.forceRemove(containerID);
         return false;
 	}
+	
+	public void menu(){
+	    Scanner sc = new Scanner(System.in);
+	    String containerID = null;
+	    String appID = null;
+	    String location = null;
+	    Integer time = null;
+	    DockerController dc = new DockerController();
+	    while(true){
+	        System.out.println("1.create\n2.start\n3.stop\n4.restart\n5.remove\n6.forceRemove\n7.inspect\ninput:");
+	        int m = sc.nextInt();
+	        sc.nextLine();
+	        switch(m){
+	            case 1:
+	                System.out.println("appID:");
+	                appID = sc.nextLine();
+	                System.out.println("location:");
+	                location = sc.nextLine();
+	                dc.create(appID, location);
+	                break;
+	            case 2:
+	                System.out.println("ContainerID:");
+	                containerID = sc.nextLine();
+	                dc.start(containerID);
+	                break;
+	            case 3:
+                    System.out.println("ContainerID:");
+                    containerID = sc.nextLine();
+                    System.out.println("time:");
+                    time = sc.nextInt();
+                    dc.stop(containerID, time);
+                    break;
+	            case 4:
+                    System.out.println("ContainerID:");
+                    containerID = sc.nextLine();
+                    System.out.println("time:");
+                    time = sc.nextInt();
+                    dc.restart(containerID, time);
+                    break;
+	            case 5:
+                    System.out.println("ContainerID:");
+                    containerID = sc.nextLine();
+                    dc.remove(containerID);
+                    break;
+	            case 6:
+                    System.out.println("ContainerID:");
+                    containerID = sc.nextLine();
+                    dc.forceRemove(containerID);
+                    break;
+	            case 7:
+                    System.out.println("ContainerID:");
+                    containerID = sc.nextLine();
+                    dc.inspect(containerID);
+                    break;
+                default:
+                    break;
+	                
+	        }
+	    }
+	}
+	public static void main(String[] args) {
+        new DockerController().menu();
+    }
 	
 }
--- src/main/java/dockercontroller/daedalus/manager/ImageManager.java
+++ src/main/java/dockercontroller/daedalus/manager/ImageManager.java
@@ -3,7 +3,7 @@ package dockercontroller.daedalus.manager;
 import dockercontroller.daedalus.service.ImageService;
 
 public class ImageManager implements ImageService{
-    private final String PHPBuildImage = "freezerglp/centos-apache-php";
+    private final String PHPBuildImage = "freezerglp/centos-apache-php:v1";
     @Override
     public String getPHPBuildImage() {
         return PHPBuildImage;
