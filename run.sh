201bf4138dae3d1b1cf236267cd4014cf81dd71b
Gong Lingpu <409349942@qq.com>
2015-08-03 19:26:34 +0800

    MOD：delete pid and authdigest file before start new httpd service

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
 #start Apache
+
+#delete the old httpd pid file
+httpdPidFile="/run/httpd/httpd.pid"
+if [ -e $httpdPidFile ];then
+	echo "remove pid file"
+	rm -f $httpdPidFile
+else
+	echo "pid file not exist"
+fi
+
+#delete the authdigest_shm file, * present the pid 
+rm -f /var/run/httpd/authdigest_shm.*
+
+#start service
 httpd
 
 #prevent the docker from stopping
-
 while true;do
 	sleep 1000;
 done
